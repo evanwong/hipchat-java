25305aae5bdb4cbcd024925881cecf1f9b84fb1c
Evan Wong <evan@evanwong.io>
2014-12-16 17:20:46 -0500

    Added delete oauth token request.

--- src/main/java/io/evanwong/oss/hipchat/v2/oauth/DeleteOauthTokenRequest.java
+++ src/main/java/io/evanwong/oss/hipchat/v2/oauth/DeleteOauthTokenRequest.java
@@ -1,4 +1,28 @@
 package io.evanwong.oss.hipchat.v2.oauth;
 
-public class DeleteOauthTokenRequest {
+import io.evanwong.oss.hipchat.v2.commons.DeleteRequest;
+import io.evanwong.oss.hipchat.v2.commons.NoContent;
+import org.apache.http.client.HttpClient;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+public class DeleteOauthTokenRequest extends DeleteRequest<NoContent> {
+
+    private final String sessionId;
+
+    DeleteOauthTokenRequest(String sessionId, String accessToken, HttpClient httpClient, ExecutorService executorService) {
+        super(accessToken, httpClient, executorService);
+        this.sessionId = sessionId;
+    }
+
+    @Override
+    protected Map<String, Object> toQueryMap() {
+        return null;
+    }
+
+    @Override
+    protected String getPath() {
+        return "/oauth/token/" + sessionId;
+    }
 }
