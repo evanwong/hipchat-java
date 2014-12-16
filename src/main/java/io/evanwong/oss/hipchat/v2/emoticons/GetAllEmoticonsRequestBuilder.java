1251e4bddfebbae8d46227c84bf00de2ded6f860
Evan Wong <evan@evanwong.io>
2014-12-16 15:58:16 -0500

    Fixed builder and unit tests.

--- src/main/java/io/evanwong/oss/hipchat/v2/emoticons/GetAllEmoticonsRequestBuilder.java
+++ src/main/java/io/evanwong/oss/hipchat/v2/emoticons/GetAllEmoticonsRequestBuilder.java
@@ -1,11 +1,12 @@
 package io.evanwong.oss.hipchat.v2.emoticons;
 
+import io.evanwong.oss.hipchat.v2.commons.ExpandableRequestBuilder;
 import io.evanwong.oss.hipchat.v2.commons.RequestBuilder;
 import org.apache.http.client.HttpClient;
 
 import java.util.concurrent.ExecutorService;
 
-public class GetAllEmoticonsRequestBuilder extends RequestBuilder<GetAllEmoticonsRequest> {
+public class GetAllEmoticonsRequestBuilder extends ExpandableRequestBuilder<GetAllEmoticonsRequestBuilder, GetAllEmoticonsRequest> {
 
     private Integer startIndex;
     private Integer maxResults;
@@ -19,24 +20,27 @@ public class GetAllEmoticonsRequestBuilder extends RequestBuilder<GetAllEmoticon
         return startIndex;
     }
 
-    public void setStartIndex(Integer startIndex) {
+    public GetAllEmoticonsRequestBuilder setStartIndex(Integer startIndex) {
         this.startIndex = startIndex;
+        return this;
     }
 
     public Integer getMaxResults() {
         return maxResults;
     }
 
-    public void setMaxResults(Integer maxResults) {
+    public GetAllEmoticonsRequestBuilder setMaxResults(Integer maxResults) {
         this.maxResults = maxResults;
+        return this;
     }
 
     public EmoticonType getType() {
         return type;
     }
 
-    public void setType(EmoticonType type) {
+    public GetAllEmoticonsRequestBuilder setType(EmoticonType type) {
         this.type = type;
+        return this;
     }
 
     @Override
